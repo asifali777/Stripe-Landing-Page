9e224559889bc28fff4ef2fe409844594f02a718
asifali777 <asifali45007@gmail.com>
2024-02-03 07:48:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("menu").addEventListener("click",()=>{
+    const navBar = document.querySelector(".nav-right");
+    navBar.classList.toggle('active');
+});document.getElementById("close").addEventListener("click",()=>{
+    const navBar = document.querySelector(".nav-right");
+    navBar.classList.toggle('active');
+});
